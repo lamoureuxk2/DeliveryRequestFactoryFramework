19476ae939e1c1205e075ea65f832913e6d31532
Karl Lamoureux <lamoureuxk@wit.edu>
2020-03-30 14:46:16 -0400

    Main successfully assigns one request. JUnit tests created that all succeed.

--- src/main/java/edu/bu/met/cs665/MainDeliveryApp.java
+++ src/main/java/edu/bu/met/cs665/MainDeliveryApp.java
@@ -1,10 +1,69 @@
 package edu.bu.met.cs665;
+import java.util.*;
+
+import org.apache.log4j.Logger;
 
 public class MainDeliveryApp {
+	
+	private static Logger logger = Logger.getLogger(MainDeliveryApp.class);
+	
+	static ArrayList<Shop> shops = new ArrayList<>();
+	static ArrayList<Driver> drivers = new ArrayList<>();
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		shops.add( new Shop("Shop1", "300 Commonwealth avenue") );
+		drivers.add( new Driver("John"));
+		drivers.add( new Driver("Mark"));
+		drivers.add( new Driver("Shawn"));
+		
+		String requestedShop = "Shop1";
+		String reqDetails = "One Large Pizza";
+		String deliveryAddress = "100 Blue Street";
+		
+		DeliveryRequest req = makeRequest(requestedShop, reqDetails, deliveryAddress);
+		
+		if(req == null) {
+			logger.error("Null DeliveryRequest, no matching shop name");
+			System.exit(1);
+		}
+		
+		if( !assignRequest(req) ) {
+			logger.info("All drivers already have requests");
+			System.exit(1);
+		}
+		
+		System.out.println("Sucessfully Assigned Request");
+		
 	}
+	
+	public static DeliveryRequest makeRequest(String shop, String desc, String addr) {
+		for(Shop s : shops) {
+			if( s.getName().equals(shop)) {
+				return s.makeRequest(desc, addr);
+			}
+		}
+		return null;
+	}
+	
+	public static boolean assignRequest(DeliveryRequest req) {
+		for( Driver d : drivers) {
+			if( d.setNewRequest(req) ) {
+				logger.info(d.getName() + " assigned Request");
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	static void clearRequests() {
+		for(Driver d : drivers) {
+			d.completeRequest();
+		}
+	}
+	
+	static void clearShops() {
+		shops.clear();
+	} 
 
 }
---
+++ src/test/java/edu/bu/met/cs665/TestDeliveryApp.java
@@ -0,0 +1,56 @@
+package edu.bu.met.cs665;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestDeliveryApp {
+	
+	static ArrayList<Shop> shops = MainDeliveryApp.shops;
+	static ArrayList<Driver> drivers = MainDeliveryApp.drivers;
+	 
+	@Test
+	public void testSuccessfulRequest() {
+		shops.clear();
+		drivers.clear();
+		shops.add( new Shop("Shop1", "300 Commonwealth avenue") );
+		drivers.add( new Driver("John"));
+		drivers.add( new Driver("Mark"));
+		drivers.add( new Driver("Shawn"));
+		
+		DeliveryRequest req = MainDeliveryApp.makeRequest("Shop1", "Pizza", "3 Blue Street");
+		assertTrue(MainDeliveryApp.assignRequest(req));
+		
+		boolean requestInDriverList = false;
+		for(Driver d : drivers) {
+			if(d.getCurrentRequest() == null) continue;
+			if(d.getCurrentRequest().equals(req)) {
+				requestInDriverList = true;
+				break;
+			}
+		}
+		assertTrue(requestInDriverList);
+	}
+	
+	@Test
+	public void testFailIfDriversOccupied() {
+		MainDeliveryApp.clearRequests();
+		MainDeliveryApp.assignRequest(MainDeliveryApp.makeRequest("Shop1", "Pizza", "3 Blue Street"));
+		MainDeliveryApp.assignRequest(MainDeliveryApp.makeRequest("Shop1", "Calzone", "5 Red Street"));
+		MainDeliveryApp.assignRequest(MainDeliveryApp.makeRequest("Shop1", "Pupusa", "7 Green Street"));
+		 
+		assertFalse(MainDeliveryApp.assignRequest(new DeliveryRequest("Shop1", "Food", "Valid addr")));
+	}
+	
+	@Test
+	public void testNullIfWrongShopName() {
+		shops.clear();
+		shops.add( new Shop("Shop1", "300 Commonwealth avenue") );
+		assertNull(MainDeliveryApp.makeRequest("WrongName", "Valid Desc", "Valid Addr"));
+		shops.clear();
+		assertNull(MainDeliveryApp.makeRequest("Shop1", "Valid Desc", "Valid Addr"));
+	}
+
+}
